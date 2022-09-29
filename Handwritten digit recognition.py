5534a640dc0983f7a8e8c49f177d4b76039b46ed
Rickwang177 <87885188+Rickwang177@users.noreply.github.com>
2022-09-30 02:14:46 +0800

    SE-Attention added
    This is an update to this project, this update content is: on the basis of the original algorithm, the attention mechanism is added, the attention mechanism uses the Squeeze-and-Excitation method, I have done the corresponding test locally, due to the selection of handwritten numeral recognition data set, the effect may not be very obvious, because it has been as high as 98%, but this is about manually adding modules. 
    And the operation of class definition and incoming parameters is a positive exploration of the changes in the network structure. Questions and discussions are welcome.

--- Handwritten digit recognition.py
+++ Handwritten digit recognition.py
@@ -67,22 +67,50 @@ print([labels[i] for i in range(64)])
 plt.imshow(img)
 
 # 模型搭建和参数优化
+
+# 输入输出相同
+
+class SElayer(torch.nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SElayer,self).__init__()
+        self.avg_pool = torch.nn.AdaptiveAvgPool2d(1)
+        self.fc = torch.nn.Sequential(
+            torch.nn.Linear(channel, channel // reduction, bias=False),
+            torch.nn.ReLU(inplace = True),
+            torch.nn.Linear(channel // reduction, channel, bias=False),
+            torch.nn.Sigmoid()
+            )
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
 class Model(torch.nn.Module):
-# __init__初学者容易误写成__int__，导致无法正确传入数据
-    def __init__(self):
+
+    def __init__(self,n_in,n_out):
         super(Model, self).__init__()
         self.conv1 = torch.nn.Sequential(
             # torch.nn.Conv2d用于搭建卷积神经网络的卷积层
             # 主要的输入参数有输入通道数，输出通道数，卷积核大小，卷积核移动步长，Padding值
             # 其中输入通道数的数据类型是整型，用于确定输入数据的层数
             # 输出通道数的数据类型也是整型，用于确定输出数据的层数
-            torch.nn.Conv2d(1,64,kernel_size=3,stride=1,padding=1),
+            # torch.nn.Conv2d(1,64,kernel_size=3,stride=1,padding=1),
+            # self.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(n_in, n_out, kernel_size=3, stride=1, padding=1),
+            # torch.nn.ReLU(),
             torch.nn.ReLU(),
-            torch.nn.Conv2d(64,128,kernel_size=3,stride=1,padding=1),
+            # torch.nn.Conv2d(64,128,kernel_size=3,stride=1,padding=1),
+            torch.nn.Conv2d(n_out, 2*n_out, kernel_size=3, stride=1, padding=1),
+            # torch.nn.ReLU(),
             torch.nn.ReLU(),
             # torch.nn.MaxPool2d用于实现卷积神经网络中的最大池化层
             # 主要的输入参数有池化窗口大小，池化窗口移动步长，Padding值
-            torch.nn.MaxPool2d(stride=2,kernel_size=2))
+            # torch.nn.MaxPool2d(stride=2,kernel_size=2))
+            torch.nn.MaxPool2d(stride=2, kernel_size=2))
+
+        self.se = SElayer(2*n_out)
 
         self.dense = torch.nn.Sequential(
             torch.nn.Linear(14*14*128,1024),
@@ -96,16 +124,22 @@ class Model(torch.nn.Module):
 
     # 前向传播forward函数
     def forward(self,x):
-        x = self.conv1(x)
+        out = self.conv1(x)
+        out = Variable(out)
+
+        out = self.se(out)
         # 对参数实现扁平化
         # 不扁平化会使得全连接层的实际输出的参数维度和其定义输入的维度将不匹配，程序会报错
-        x = x.view(-1,14*14*128)
+        out = out.view(-1,14*14*128)
         # 进行最后的分类
-        x = self.dense(x)
-        return x
+        out = self.dense(out)
+
+        return out
 
 #模型的训练和参数优化
-model = Model().to(device)
+n_in = 1
+n_out = 64
+model = Model(n_in,n_out).to(device)
 # 计算损失值的损失函数用的是交叉熵
 cost = torch.nn.CrossEntropyLoss()
 cost = cost.to(device)
